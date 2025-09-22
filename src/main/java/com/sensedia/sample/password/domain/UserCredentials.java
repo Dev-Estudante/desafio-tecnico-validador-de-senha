6e92c7d251ac40ca1193962f8a6b2454da56292b
Marcky404 <ianalves908@gmail.com>
2025-09-22 02:18:56 -0300

    refactor: alteração do atributo password de String para List<String> para registrar histórico de senhas

--- src/main/java/com/sensedia/sample/password/domain/UserCredentials.java
+++ src/main/java/com/sensedia/sample/password/domain/UserCredentials.java
@@ -2,10 +2,10 @@ package com.sensedia.sample.password.domain;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
@@ -13,12 +13,13 @@ import org.springframework.data.mongodb.core.mapping.MongoId;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Document
+@Document("userCredentials")
+@Builder
 public class UserCredentials {
 
     @MongoId
     private String id;
-    private String username;
-    private String password;
+    private String user;
+    private List<String> passwords;
 
 }
