38f55bc8e30386354d85cd47c63871eb04cb8e14
Dev-Estudante <devestudante5@gmail.com>
2025-09-22 11:51:12 -0300

    refctore: adicionado o @Build no UserCredentialsDto

--- src/main/java/com/sensedia/sample/password/domain/dto/UserCredentialsDTO.java
+++ src/main/java/com/sensedia/sample/password/domain/dto/UserCredentialsDTO.java
@@ -3,6 +3,7 @@ package com.sensedia.sample.password.domain.dto;
 import com.sensedia.sample.password.domain.UserCredentials;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class UserCredentialsDTO {
 
     private String user;
@@ -26,3 +28,4 @@ public class UserCredentialsDTO {
     }
 
 }
+
