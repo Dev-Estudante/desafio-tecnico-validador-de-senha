8296c638111e8463f3542fb9ab7e51b89d987977
Marcky404 <ianalves908@gmail.com>
2025-09-22 02:31:32 -0300

    feat: refatoração do método save para validar senha e registrar histórico

--- src/main/java/com/sensedia/sample/password/service/UserCredentialsService.java
+++ src/main/java/com/sensedia/sample/password/service/UserCredentialsService.java
@@ -2,7 +2,10 @@ package com.sensedia.sample.password.service;
 
 
 import com.sensedia.sample.password.domain.UserCredentials;
+import com.sensedia.sample.password.domain.dto.UserCredentialsDTO;
 import com.sensedia.sample.password.repository.UserCredentialsRepository;
+import com.sensedia.sample.password.utils.PasswordValidate;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,7 +17,24 @@ public class UserCredentialsService {
 
     private final UserCredentialsRepository repository;
 
-    public void save(UserCredentials credentials){
-        repository.save(credentials);
+    public void save(UserCredentialsDTO request) {
+
+        PasswordValidate.validate(request.getUser(), request.getPassword(), request.getConfirmPassword());
+        UserCredentials userCredentials = UserCredentialsDTO.toUserCredentials(request);
+
+        var savedUser = findByUser(userCredentials.getUser());
+
+        if (savedUser.getUser() != null) {
+            List<String> passwords = PasswordValidate.checkPasswordReuse(request.getPassword(), savedUser.getPasswords());
+            savedUser.setPasswords(passwords);
+            repository.save(savedUser);
+        }
+
+        repository.save(userCredentials);
     }
+
+    public UserCredentials findByUser(String user) {
+        return repository.findByUser(user).orElse(new UserCredentials());
+    }
+
 }
