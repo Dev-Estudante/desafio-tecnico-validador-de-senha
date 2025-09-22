9fe226234d7f3b29451acab1cbdb05c1d84c81b9
Marcky404 <ianalves908@gmail.com>
2025-09-22 02:30:26 -0300

    feat: adicionado método findByUser

--- src/main/java/com/sensedia/sample/password/repository/UserCredentialsRepository.java
+++ src/main/java/com/sensedia/sample/password/repository/UserCredentialsRepository.java
@@ -1,7 +1,11 @@
 package com.sensedia.sample.password.repository;
 
 import com.sensedia.sample.password.domain.UserCredentials;
-import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserCredentialsRepository extends CrudRepository<UserCredentials, String> {
+public interface UserCredentialsRepository extends MongoRepository<UserCredentials, String> {
+
+    Optional<UserCredentials> findByUser(String user);
+
 }
